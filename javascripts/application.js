f0b30de13f1d5413aa0f3cdec4ba234011e71af2
Fernando Blat <ferblape@gmail.com>
2014-09-26 15:42:41 +0200

    First commit. Folder structure

---
+++ javascripts/application.js
@@ -0,0 +1,20 @@
+var position = [42.571716599999990000, -0.547055399999976500];
+
+function showGoogleMaps() {
+
+  var latLng = new google.maps.LatLng(position[0], position[1]);
+
+  var mapOptions = {
+    zoom: 13,
+    streetViewControl: false, // hide the yellow Street View pegman
+    scaleControl: true, // allow users to zoom the Google Map
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: latLng,
+    draggable: true
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', showGoogleMaps);
