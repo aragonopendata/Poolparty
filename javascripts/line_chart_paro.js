6f8340723aa3668bc828345be6a8e6b30fe934c7
Fernando Blat <ferblape@gmail.com>
2014-09-27 12:05:15 +0200

    Inserted line chart unemployment

--- javascripts/line_chart_paro.js
+++ javascripts/line_chart_paro.js
@@ -1,129 +1,152 @@
-var margin = {top: 20, right: 120, bottom: 30, left: 50},
-    width = 550 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+'use strict';
 
-var parseDate = d3.time.format("%Y").parse;
+(function(){
+  var margin = {
+    top: 20,
+    right: 15,
+    bottom: 30,
+    left: 45
+  };
 
-var x = d3.time.scale()
-    .range([0, width]);
+  var width = 550 - margin.left - margin.right;
+  var height = 400 - margin.top - margin.bottom;
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+  var parseDate = d3.time.format("%Y").parse;
 
-var color = d3.scale.ordinal()
-    .range(["#001c9c","#101b4d","#475003","#9c8305","#d3c47c"]);
+  var x = d3.time.scale()
+      .range([0, width]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+  var y = d3.scale.linear()
+      .range([height, 0]);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+  var color = d3.scale.ordinal()
+      .range(['#B3DE69', '#FCCDE5', '#D9D9D9', '#BC80BD', '#CCEBC5', '#FFED6F']);	
 
-var line = d3.svg.line()
-    .interpolate("linear")
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.pop); });
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
 
-var svg = d3.select(".padron").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
 
-// Creamos una etiqueta DIV que utilizaremos a modo de tooltip.
-var tooltip_padron = d3.select(".padron")
-                .append("div")
-                .attr("class", "tooltip")
-                .style("position", "absolute")
-                .style("z-index", "20")
-                .style("top", (height + 30) +"px")
-                .style("left", "500px")
-                .html("<p>45983<br>Población total</p>");   
+  var line = d3.svg.line()
+      .interpolate("linear")
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.pop); });
 
+  var svg = d3.select(".unemployment").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("poblacion_paro_jaca.csv", function(error, data) {
-  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+  var tooltip = svg
+    .append('text')
+    .attr({'text-anchor': 'middle'})
+    .attr({'x': (width + margin.right)/2, 'y': height - margin.top - 30})
+    .style({'font-size': '26px', 'font-weight': 'bold', 'fill': '#aaa'});
 
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-  });
-  console.log(data);
-  var padron = color.domain().map(function(name) {
-    return {
-      name: name,
-      values: data.map(function(d) {
-        return {date: d.date, pop: +d[name]};
-      })
-    };
-  });
-  padron = padron.filter(function (d) {return d.name !== "cod" && d.name !== "mun";});
-  
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain([0,
-    // d3.min(padron, function(c) { return d3.min(c.values, function(v) { return v.pop; }); }),
-    d3.max(padron, function(c) { return d3.max(c.values, function(v) { return v.pop; }); })
-  ]);
-  
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("transform", "translate(" + width + ",0)")
-      .attr("text-anchor", "end")
-      .attr("dy", "-.71em")
-      .text("Año");
+  function setTooltipTotal(tooltip, data){
+    var unemploymentTotal = data.filter(function(d) {
+      return d.name === 'Paro';
+    })[0]; // NOTE: Returns an array with one value
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Población");
+    var lastYear  = data.filter(function(d) {
+      return d.name === 'Paro';
+    })[0].values[unemploymentTotal.values.length - 1].date.getFullYear();
 
-  var city = svg.selectAll(".city")
-      .data(padron)
-    .enter().append("g")
-      .attr("class", "city");
+    tooltip.html('Paro en ' + lastYear + ': ' + unemploymentTotal.values[unemploymentTotal.values.length - 1].pop);
+  }
 
-  city.append("path")
-      .attr("class", "line")
-      .attr("d", function(d) { return line(d.values); })
-      .style("stroke", function(d) { return color(d.name); });
+  d3.csv("/assets/poblacion_paro_jaca.csv", function(error, data) {
+    color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
 
-  city.append("text")
-      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.pop) + ")"; })
-      .attr("x", 3)
-      .attr("dy", ".35em")
-      .attr("dx", ".35em")
-      .text(function(d) { return d.name; });
+    data.forEach(function(d) {
+      d.date = parseDate(d.date);
+    });
+    var padron = color.domain().map(function(name) {
+      return {
+        name: name,
+        values: data.map(function(d) {
+          return {date: d.date, pop: +d[name]};
+        })
+      };
+    });
+    padron = padron.filter(function (d) {return d.name !== "cod" && d.name !== "mun";});
 
-  var point = city.append("g")
-      .attr("class", "line-point");
+    console.log(padron);
+    
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([0,
+      // d3.min(padron, function(c) { return d3.min(c.values, function(v) { return v.pop; }); }),
+      d3.max(padron, function(c) { return d3.max(c.values, function(v) { return v.pop; }); })
+    ]);
+    
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .append("text")
+        .attr("transform", "translate(" + width + ",0)")
+        .attr("text-anchor", "end")
+        .attr("dy", "-.71em")
+        .text("Año");
 
-  point.selectAll('circle')
-      .data(function(d) { return d.values})
-      .enter().append('circle')
-      .attr("cx", function(d) { return x(d.date) })
-      .attr("cy", function(d) { return y(d.pop) })
-      .attr("r", 3.5)
-      .style("fill", function(d) { return color(this.parentNode.__data__.name); })
-      .on("mouseover", function (d, i) {
-                d3.select(this)
-                  .style("stroke-width", 0.5)
-                  .style("stroke", "red");
-                tooltip_padron.html( "<p>Población año: " + d.date + "<br>" + d.pop + "</p>" );
-                    })
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Población");
+
+    var city = svg.selectAll(".city")
+        .data(padron)
+      .enter().append("g")
+        .attr("class", "city")
+      .attr('data-legend-color', function(d) {
+        return color(d.name);
+      });
+
+    city.append("path")
+        .attr("class", "line")
+        .attr("d", function(d) { return line(d.values); })
+        .style("stroke", function(d) { return color(d.name); });
+
+    var point = city.append("g")
+        .attr("class", "line-point");
+
+    point.selectAll('circle')
+        .data(function(d) { return d.values})
+        .enter().append('circle')
+        .attr("cx", function(d) { return x(d.date) })
+        .attr("cy", function(d) { return y(d.pop) })
+        .attr('r', 6)
+        .style("fill", function(d) { return color(this.parentNode.__data__.name); })
+        .on("mouseover", function (d, i) {
+          d3.select(this)
+            .style("stroke-width", 0.5)
+            .style("stroke", "red");
+          tooltip.text(this.parentNode.__data__.name + ' en ' + d.date.getFullYear() + ': ' + d.pop);
+        })
         .on("mouseout", function (d) {
           d3.select(this)
             .style("stroke-width", 0);
-          tooltip.html("Nº empresas total: 432")
+          setTooltipTotal(tooltip, padron);
         });
+        setTooltipTotal(tooltip, padron);
 
-});
+    /* Legend */
+    city.attr('data-legend',function(d) { return d.name;});
+
+    svg.append('g')
+      .attr('class', 'legend')
+      .attr('transform', 'translate(400,190)')
+      .style('font-size', '12px')
+      .call(d3.legend);
+    /* end legend */
+  });
+})();
