0c9d55f2cf3509550031c9002ac98a9e83eef196
Fernando Blat <ferblape@gmail.com>
2014-09-27 14:19:50 +0200

    Encoding

--- active_csv.rb
+++ active_csv.rb
@@ -4,7 +4,7 @@ class ActiveCsv
   def open(path)
     @csv = {}
 
-    CSV.read(path, headers: true).each do |row|
+    CSV.read(path, headers: true, encoding: 'windows-1251:UTF-8').each do |row|
       @csv[row['ine']] = {
         name:    row['name'],
         ine:     row['ine'],
